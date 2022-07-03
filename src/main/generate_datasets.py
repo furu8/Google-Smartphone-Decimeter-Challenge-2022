52509eda14602ad1788283cb71fb1cb9c3712c0c
furu8 <foooolurk1125@gmail.com>
2022-07-03 15:11:53 +0900

    feat:imu分解

--- src/main/generate_datasets.py
+++ src/main/generate_datasets.py
@@ -32,24 +32,50 @@ tr_paths = [np.unique(np.array(places_path)) for places_path in make_paths_each_
 te_paths = [np.unique(np.array(places_path)) for places_path in make_paths_each_place(tests)]
 tr_paths[0:5]
 # %%
+# 結合の識別元
+def naming_collection_and_phone(df, path):
+    df['collectionName'] = path.split('\\')[-1].split('/')[0]
+    df['phoneName'] = path.split('/')[-2]
+    return df
+
 def generate_datasets(paths, dir_name):
-    gnss_list, imu_list, gt_list = [], [], []
+    gnss_list, gt_list = [], []
+    mag_list, acc_list, gyro_list = [], [], []
     for phone_paths in tqdm(paths):
         for path in phone_paths:
-            gnss_list.append(pd.read_csv(f'{path}/device_gnss.csv'))
-            imu_list.append(pd.read_csv(f'{path}/device_imu.csv'))
+            gnss_df = pd.read_csv(f'{path}/device_gnss.csv')
+            gnss_df = naming_collection_and_phone(gnss_df, path)
+            gnss_list.append(gnss_df)
+
+            imu_df = pd.read_csv(f'{path}/device_imu.csv')
+            imu_df = naming_collection_and_phone(imu_df, path)
+            mag_df = imu_df[imu_df['MessageType']=='UncalMag']
+            acc_df = imu_df[imu_df['MessageType']=='UncalAccel']
+            gyro_df = imu_df[imu_df['MessageType']=='UncalGyro']
+            mag_list.append(mag_df)
+            acc_list.append(acc_df)
+            gyro_list.append(gyro_df)
+
             if dir_name == 'train':
-                gt_list.append(pd.read_csv(f'{path}/ground_truth.csv'))
+                gt_df = pd.read_csv(f'{path}/ground_truth.csv')
+                gt_df = naming_collection_and_phone(gt_df, path)
+                gt_list.append(gt_df)
 
         place = path.split('US-')[-1].split('/')[0]
 
-        gnss_df = pd.concat(gnss_list)
-        gnss_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_gnss.csv', index=False)
-        imu_df = pd.concat(imu_list)
-        imu_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_imu.csv', index=False)
+        concated_gnss_df = pd.concat(gnss_list)
+        concated_gnss_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_gnss.csv', index=False)
+
+        concated_mag_df = pd.concat(mag_list)
+        concated_acc_df = pd.concat(acc_list)
+        concated_gyro_df = pd.concat(gyro_list)
+        concated_mag_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_mag.csv', index=False)
+        concated_acc_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_acc.csv', index=False)
+        concated_gyro_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_gyro.csv', index=False)
+
         if dir_name == 'train':
-            gt_df = pd.concat(gt_list)
-            gt_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_gt.csv', index=False)
+            concated_gt_df = pd.concat(gt_list)
+            concated_gt_df.to_csv(f'{DATA_PATH}/shilver/{dir_name}/{place}_gt.csv', index=False)
 
 # %%
 # generate train
